296fb51f9a012a142ff92cae6ec53496029e77f1
WICKLES DE OLIVEIRA CORREIA <wickles.ocorreia@senacsp.edu.br>
2024-08-28 17:12:43 -0300

    criado os getters, setters e function construct

--- models/ContaEspecial.php
+++ models/ContaEspecial.php
@@ -1,9 +1,7 @@
 <?php
-
+namespace App\Models;
 class ContaEspecial{
-//Criar uma classe chamada ContaEspecial, com os atributos titular, numero da conta, saldo e limite. 
-//Criar os getters e setters somente quando necessário. Criar para esta classe um construtor customizado, 
-//que já receba os dados do objeto na sua criação. 
+
     private string $titular; 
     private string $numeroConta;
 
@@ -11,6 +9,46 @@ class ContaEspecial{
 
     private float $limite;
 
-    
+    public function __construct(string $titular, string $numeroConta, float $saldo, float $limite) {
+        $this->setTitular($titular);
+        $this->setNumeroConta($numeroConta);
+        $this->setSaldo($saldo);
+        $this->setLimite($limite);
+    }   
 
+    public function getTitular(): string {
+    	return $this->titular;
+    }
+
+
+    public function setTitular(string $titular): void {
+    	$this->titular = $titular;
+    }
+
+
+    public function getNumeroConta(): string {
+    	return $this->numeroConta;
+    }
+
+
+    private function setNumeroConta(string $numeroConta): void {
+    	$this->numeroConta = $numeroConta;
+    }
+
+    public function getSaldo() {
+    	return number_format($this->saldo,2);
+    }
+
+    public function setSaldo(float $saldo): void {
+    	$this->saldo = $saldo;
+    }
+
+
+    public function getLimite(){
+    	return number_format($this->limite,2);
+    }
+
+    public function setLimite(float $limite): void {
+    	$this->limite = $limite;
+    }
 }
