9b8b1780fc456b8cc1c018dc8c6b65760d675c92
WICKLES DE OLIVEIRA CORREIA <wickles.ocorreia@senacsp.edu.br>
2024-08-28 15:25:16 -0300

    commit inicial, criando class e seus atributos

---
+++ models/ContaEspecial.php
@@ -0,0 +1,16 @@
+<?php
+
+class ContaEspecial{
+//Criar uma classe chamada ContaEspecial, com os atributos titular, numero da conta, saldo e limite. 
+//Criar os getters e setters somente quando necessário. Criar para esta classe um construtor customizado, 
+//que já receba os dados do objeto na sua criação. 
+    private string $titular; 
+    private string $numeroConta;
+
+    private float $saldo;
+
+    private float $limite;
+
+    
+
+}
